20fbbd30d470ea33521a4ad76eb8e058a4642568
Bo Zhang <bo@gradle.com>
2023-08-29 15:59:56 +0800

    Update DSL version to 2023.05

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
 
-version = "2021.2"
+version = "2023.05"
 
 project(Project)
