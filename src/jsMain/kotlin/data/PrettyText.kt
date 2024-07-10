e0a1fc4ec0768acb3c82c3db2c5e89dd38fa0c7e
Alex Semin <asemin@gradle.com>
2024-07-10 23:17:30 +0200

    Add builder for PrettyText

--- src/jsMain/kotlin/data/PrettyText.kt
+++ src/jsMain/kotlin/data/PrettyText.kt
@@ -11,10 +11,26 @@ data class PrettyText(val fragments: List<Fragment>) {
         data class Reference(val name: String) : Fragment()
     }
 
+    class Builder {
+
+        private
+        val fragments = mutableListOf<Fragment>()
+
+        fun text(text: String) = apply { fragments.add(Fragment.Text(text)) }
+
+        fun ref(reference: String) = apply { fragments.add(Fragment.Reference(reference)) }
+
+        fun build() = PrettyText(fragments.toList())
+    }
+
     companion object {
 
         fun ofText(text: String): PrettyText {
             return PrettyText(listOf(Fragment.Text(text)))
+        }
+
+        fun build(builder: Builder.() -> Unit): PrettyText {
+            return Builder().apply(builder).build()
         }
     }
 }
