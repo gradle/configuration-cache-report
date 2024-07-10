34a924d627845b549647531325009ad282fc3712
Alex Semin <asemin@gradle.com>
2024-07-10 20:37:13 +0200

    Extract PrettyText component

--- src/jsMain/kotlin/report/PrettyText.kt
+++ src/jsMain/kotlin/report/PrettyText.kt
@@ -1,5 +1,14 @@
 package report
 
+import elmish.Component
+import elmish.View
+import elmish.attributes
+import elmish.code
+import elmish.empty
+import elmish.small
+import elmish.span
+
+
 internal
 data class PrettyText(val fragments: List<Fragment>) {
 
@@ -9,4 +18,46 @@ data class PrettyText(val fragments: List<Fragment>) {
 
         data class Reference(val name: String) : Fragment()
     }
+}
+
+
+internal
+class PrettyTextComponent<Intent>(
+    private val copyableReferences: Boolean,
+    private val getCopyIntent: (String) -> Intent,
+) : Component<PrettyText, Intent> {
+
+    override fun view(model: PrettyText): View<Intent> = viewPrettyText(model)
+
+    override fun step(intent: Intent, model: PrettyText): PrettyText = model
+
+    private
+    fun viewPrettyText(model: PrettyText): View<Intent> = span(
+        model.fragments.map {
+            when (it) {
+                is PrettyText.Fragment.Text -> span(it.text)
+                is PrettyText.Fragment.Reference -> reference(it.name, copyableReferences)
+            }
+        }
+    )
+
+    private
+    fun reference(name: String, isCopyable: Boolean): View<Intent> = span(
+        invisibleBacktick(),
+        code(name),
+        invisibleBacktick(),
+        if (!isCopyable) empty else copyButton(
+            text = name,
+            tooltip = "Copy reference to the clipboard"
+        )
+    )
+
+    private
+    fun copyButton(text: String, tooltip: String): View<Intent> = small(
+        attributes {
+            title(tooltip)
+            className("copy-button")
+            onClick { getCopyIntent(text) }
+        }
+    )
 }
