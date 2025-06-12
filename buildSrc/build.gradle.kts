2bdd427053c762cac62b870b9be2683931d428f8
Reinhold Degenfellner <rdegenfellner@gradle.com>
2025-06-12 16:36:26 +0200

    adopt to gradle 9

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 dependencies {
     implementation(gradleApi())
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25") {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21") {
         because("this includes the multiplatform plugin")
     }
     implementation("org.gradle.kotlin:gradle-kotlin-dsl-conventions:0.9.0")
