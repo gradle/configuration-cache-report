df0542b44487af3e59395df32a138743a38306b0
Christoph Obexer <cobexer@gradle.com>
2026-03-24 11:12:30 +0100

    TeamCity: Update to current DSL version
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,6 @@
-import jetbrains.buildServer.configs.kotlin.v2019_2.project
-import jetbrains.buildServer.configs.kotlin.v2019_2.version
+import jetbrains.buildServer.configs.kotlin.project
+import jetbrains.buildServer.configs.kotlin.version
 
-version = "2023.05"
+version = "2025.11"
 
 project(Project)
